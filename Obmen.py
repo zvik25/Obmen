cbf94cf44ca06ab070a19815390101390c64192f
zvik25 <zvik@mail.ru>
2025-08-03 19:07:01 +0300

    Создана функция exchange для получения курса валют из Интернета.

--- Obmen.py
+++ Obmen.py
@@ -12,6 +12,28 @@ from tkinter import messagebox as mb
 # p.print(data) #печать в столбик данных json файла. Печать print() - в одну строчку вывод данных json файла.
 """Создаем оконный интерфейс"""
 
+def exchange():
+    code = entry.get()
+
+    if code: #проверка для введенного значения кода
+        try:
+            response = requests.get('https://open.er-api.com/v6/latest/USD')
+            response.raise_for_status() #проверка доступности-работы сайта
+            data = response.json()
+            if code in data['rates']: #проверка кода валюты - есть, правильно - rates is from json
+                exchange_rate = data['rates'][code] #code here is a key - for json file data
+                mb.showinfo('Курс обмена', f'Курс: {exchange_rate} {code} за 1 доллар')
+            else:
+                mb.showerror('Ошибка', f'Валюта {code} не найдена!')
+        except Exception as e:
+            mb.showerror('Ошибка', f'Произошла ошибка: {e}')
+
+    else: # if code not entered
+        mb.showwarning('Внимание', "Введите код валюты.")
+
+
+
+
 
 window = Tk()
 window.title('Курсы обмена валют')
@@ -22,7 +44,7 @@ Label(text='Введите код валюты').pack(padx=10, pady=10) #не б
 entry=Entry()
 entry.pack(padx=10, pady=10)
 
-Button(text'Получить курс обмена к доллару', command=exchange.pack(padx=10, pady=10))
+Button(text='Получить курс обмена к доллару', command=exchange).pack(padx=10, pady=10)
 
 
 window.mainloop()
