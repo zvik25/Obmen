2232deaa453f9eb7ca57021713994233a264ecae
zvik25 <zvik@mail.ru>
2025-08-03 19:27:42 +0300

    Вместо поля ввода добавлен выпадающий список

--- Obmen.py
+++ Obmen.py
@@ -2,6 +2,7 @@ import requests
 import json
 from tkinter import *
 from tkinter import messagebox as mb
+from tkinter import ttk
 
 # import pprint
 #
@@ -9,11 +10,12 @@ from tkinter import messagebox as mb
 # data = json.loads(result.text) # to get json format data
 # p = pprint.PrettyPrinter(intent=4)
 #
-# p.print(data) #печать в столбик данных json файла. Печать print() - в одну строчку вывод данных json файла.
-"""Создаем оконный интерфейс"""
+# p.print(data) #печатает в столбик данные json файла. Печать print() - в одну строчку вывод данных json файла.
+
 
 def exchange():
-    code = entry.get()
+    # code = entry.get()
+    code = combobox.get()
 
     if code: #проверка для введенного значения кода
         try:
@@ -34,15 +36,20 @@ def exchange():
 
 
 
-
+"""Создаем оконный интерфейс"""
 window = Tk()
 window.title('Курсы обмена валют')
 window.geometry('360x180')
 
-Label(text='Введите код валюты').pack(padx=10, pady=10) #не будет использоваться => переменную не задаем
+# Label(text='Введите код валюты').pack(padx=10, pady=10) #не будет использоваться => переменную не задаем
+Label(text='Выберите код валюты').pack(padx=10, pady=10) #не будет использоваться => переменную не задаем #For combobox
 
-entry=Entry()
-entry.pack(padx=10, pady=10)
+cur = ['RUB', 'BGP', 'EUR', 'JPY', 'CNY', 'KZT', 'UZS', 'CHF', 'AED', 'CAD'] #список кодов для combobox'a
+combobox = ttk.Combobox(values=cur)
+combobox.pack(padx=10, pady=10)
+
+# entry=Entry() # не нужны из-за combobox
+# entry.pack(padx=10, pady=10)
 
 Button(text='Получить курс обмена к доллару', command=exchange).pack(padx=10, pady=10)
 
